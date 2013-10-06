e22327746e6a98721f12077d51b1aa0d84177466
codef0rmer <amit.2006.it@gmail.com>
2013-10-06 11:58:52 +0530

    v1.0.2 - Fix reader link position because of Google`s new app launcher (http://goo.gl/isjpY4)

--- chrome-extension/js/app.js
+++ chrome-extension/js/app.js
@@ -1,14 +1,7 @@
 function loadReader(name, url) {
-  var $link = $('#gb_32');
+  var $pluslink = $('a[data-ved="0CAIQwi4oAA"]');
 
-  if ($link.length === 1) {
-    $link.removeAttr('onclick').unbind('click').attr({
-      'href': url,
-      'title': name
-    });
-  } else {
-    $('<li class="gbmtc"><a class="gbmt" id="after_reader" href="' + url +'" title="' + name + '">Reader</a></li>').insertAfter($('#gb_30').parent());
-  }
+  $('<div class="gb_e gb_f" style="display:block"><a id="after_reader" class="gb_b" href="' + url +'" title="' + name + '" target="_blank">Reader</a></div>').insertAfter($pluslink.parent());
 }
 
 // sends a request to background.js
